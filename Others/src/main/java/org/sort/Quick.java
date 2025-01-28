1e031eda82eebedf0187fa0fec64be8a93682bc3
Devlion59 <cheremnyhdn@gmail.com>
2025-01-28 23:10:12 +0500

    quit sort alg done

---
+++ Others/src/main/java/org/sort/Quick.java
@@ -0,0 +1,73 @@
+package org.sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Quick {
+
+    public static int partition(int[] arr, int low, int high) {
+
+        int index = high;
+        int pivot = arr[index];
+
+            for (int i = low; i < index;) {
+                if (arr[i] > pivot) {
+                    swap(arr, index-1, index);
+                    if ((index - i) > 1){
+                        swap(arr, i, index);
+                    }
+                    --index;
+                } else
+                    ++i;
+            }
+        return index;
+
+
+        /*int pivot = arr[high];
+        int i = (low);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+            }
+        }
+
+
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+
+
+        return i;*/
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+
+    public static void quickSort(int[] arr, int low, int high) {
+
+        if (low < high) {
+
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {17, 14, 15, 28, 30, 8, -6, 1, 43, 18};
+        int[] arr2 = {17, 14, 15, 28, 30, 8, -6, 1, 43, 18};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
